b8633713184f859f7660546e8caa3915f1a2c983
Ken Nishimura <ken97531@gmail.com>
2010-10-21 10:33:48 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Worklista::Application.config.secret_token = 'e0e8be80529075c898e44e263897e0c43637f656bf14eaeca5399d0306ab79fa46a1f5ca379918c1428e86c738953425777611b59dadc69b518f99f7ec4adb28'
