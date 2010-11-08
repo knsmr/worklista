299e3d3bf0b4201657b41ffedea6cf3c657e15a7
Ken Nishimura <ken97531@gmail.com>
2010-11-08 22:30:12 +0900

    hatena link helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def link_to_hatena(url)
+    base_url = "http://b.hatena.ne.jp/entry/"
+    if url =~ /^(http:\/\/)(.+)/ then
+      target = $2
+    else
+      target = ""
+    end
+    base_url + target 
+   end
+
 end
