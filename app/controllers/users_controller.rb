a32ed39b694f21e9fe61c03e753cc86cb50a5b27
Ken Nishimura <ken97531@gmail.com>
2010-11-13 16:35:30 +0900

    cosmetic modify

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,14 +6,16 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find_by_username(params[:username])
-    @items = @user.items.paginate :page => params[:page], :per_page => 10
+    @items = @user.items.reverse.paginate :page => params[:page],
+                                          :per_page => 10
     render "me"
   end
 
   def me
     if user_signed_in?
       @user = current_user
-      @items = @user.items.paginate :page => params[:page], :per_page => 10
+      @items = @user.items.reverse.paginate :page => params[:page],
+                                            :per_page => 10
       render "me"
     else
       redirect_to users_path
