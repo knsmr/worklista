997571d240fdbfff643ca89631f6805ef4325043
Ken Nishimura <ken97531@gmail.com>
2010-11-14 18:57:55 +0900

    some css changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,9 @@ class UsersController < ApplicationController
   def me_popular
     if user_signed_in?
       @user = current_user
-      @items = @user.items.order("hatena").reverse.paginate :page => params[:page], :per_page => 10
+      @items = @user.items
+                    .order("hatena").reverse
+                    .paginate :page => params[:page], :per_page => 10
       render "me"
     else
       redirect_to users_path
