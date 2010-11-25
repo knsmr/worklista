7b93ecc5eba703884486a0458e6a04ebcdf718f0
Ken Nishimura <ken97531@gmail.com>
2010-11-25 12:06:00 +0900

    cant use int for boolean in migration file

--- db/migrate/20101115013947_add_pick.rb
+++ db/migrate/20101115013947_add_pick.rb
@@ -1,6 +1,6 @@
 class AddPick < ActiveRecord::Migration
   def self.up
-    add_column :items, :pick, :boolean, :default => 0
+    add_column :items, :pick, :boolean, :default => false
   end
 
   def self.down
