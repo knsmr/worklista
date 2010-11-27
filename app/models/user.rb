b27b78f194edd4ec0c11a2aa844ba1f6ffdf3da7
Ken Nishimura <ken97531@gmail.com>
2010-11-27 19:07:05 +0900

    cosmetic change

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,18 +12,18 @@ class User < ActiveRecord::Base
 
   validates_uniqueness_of :username
 
-  # Override login key to use username OR email
-  def self.find_for_database_authentication(conditions)
-    value = conditions[authentication_keys.first]
-    where(["username = :value OR email = :value", { :value => value }]).first
-  end
-
   has_attached_file :photo,
                     :styles => { :medium => "240x240", :thumb => "80x80",
                                  :original => "300x300"}, 
                     :default_url => "/images/missing-:style.png"
 
   validates_attachment_size :photo, :less_than => 1.megabytes
+
+# Override login key to use username OR email
+  def self.find_for_database_authentication(conditions)
+    value = conditions[authentication_keys.first]
+    where(["username = :value OR email = :value", { :value => value }]).first
+  end
 
 # to allow users to edit their accout without providing a password
 
