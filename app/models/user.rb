a518181241ac1c5c630bf92de710e3d2cf09ab3a
Ken Nishimura <ken97531@gmail.com>
2010-11-27 18:49:33 +0900

    to allow users to edit their account without providing a password

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,4 +26,14 @@ class User < ActiveRecord::Base
 
   validates_attachment_size :photo, :less_than => 2.megabytes
 
+# to allow users to edit their accout without providing a password
+
+  def update_with_password(params={}) 
+    if params[:password].blank? 
+      params.delete(:password) 
+      params.delete(:password_confirmation) if params[:password_confirmation].blank? 
+    end 
+    update_attributes(params) 
+  end 
+
 end
