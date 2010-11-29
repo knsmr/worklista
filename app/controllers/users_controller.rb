291c27f418e1df71183ef6c34898a5c52b702289
Ken Nishimura <ken97531@gmail.com>
2010-11-29 20:41:03 +0900

    show which tab is selected

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,18 +8,24 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by_username(params[:username])
     @items = @user.items.order("published_at").reverse.paginate :page => params[:page], :per_page => PAGINATION
+
+    @select = :recent
     render "me"
   end
 
   def popular
     @user = User.find_by_username(params[:username])
     @items = @user.items.order("hatena").reverse.paginate :page => params[:page], :per_page => PAGINATION
+
+    @select = :popular
     render "me"
   end
 
   def pickup
     @user = User.find_by_username(params[:username])
     @items = @user.items.order("published_at").reverse.find_all{|i| i.pick}.paginate :page => params[:page], :per_page => PAGINATION
+
+    @select = :pickup
     render "me"
   end
 
