d91792c07cf4bf271a9145cb23a70e8e7e11a3c8
Makoto Inoue <inouemak@googlemail.com>
2010-12-26 23:40:32 +0000

    First passing spec on item controller

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe ItemsController do
+  include Devise::TestHelpers # to give your spec access to helpers
 
+  before(:each) do
+    @user = Factory(:user)
+    sign_in :user, @user
+  end
+
+  describe "#create" do
+    it "should create new item" do
+      expect{
+        post :create,
+             :user_id => @user.id,
+             :item => {:url => 'http://www.google.com'}
+      }.to change(Item, :count).by(1)
+    end
+  end
 end
