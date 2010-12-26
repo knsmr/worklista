09aa0f8c21960463076b7edebe6dc387cead665b
Makoto Inoue <inouemak@googlemail.com>
2010-12-26 23:57:38 +0000

    Added spec for redirection and small refactoring

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -5,16 +5,30 @@ describe ItemsController do
 
   before(:each) do
     @user = Factory(:user)
+    @params = {
+      :user_id => @user.id,
+      :item => {:url => 'http://www.google.com'}
+    }
     sign_in :user, @user
   end
 
   describe "#create" do
     it "should create new item" do
       expect{
-        post :create,
-             :user_id => @user.id,
-             :item => {:url => 'http://www.google.com'}
+        post :create, @params
       }.to change(Item, :count).by(1)
+    end
+
+    it "should redirect to edit page" do
+      post :create, @params
+      response.should redirect_to edit_user_item_path(@user, Item.last)
+    end
+    
+    it "should assign variables" do
+      post :create, @params
+      flash[:notice].should == "Created an item. Any changes?"
+      assigns[:user].should == @user
+      assigns[:item].should_not be_nil
     end
   end
 end
