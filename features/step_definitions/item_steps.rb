87969b34c86a8c5c218caadc525d1a96d003228c
Makoto Inoue <inouemak@googlemail.com>
2010-12-26 23:42:08 +0000

    Removed unnused specs and steps

--- features/step_definitions/item_steps.rb
+++ features/step_definitions/item_steps.rb
@@ -1,18 +1,3 @@
-Given /^the following items:$/ do |items|
-  Item.create!(items.hashes)
-end
-
-When /^I delete the (\d+)(?:st|nd|rd|th) item$/ do |pos|
-  visit items_path
-  within("table tr:nth-child(#{pos.to_i+1})") do
-    click_link "Destroy"
-  end
-end
-
-Then /^I should see the following items:$/ do |expected_items_table|
-  expected_items_table.diff!(tableish('table tr', 'td,th'))
-end
-
 Given /^I am logged in as "([^"]*)"$/ do |arg1|
   Given %{I am on the home page}
   And   %{I follow "Sign up"}
