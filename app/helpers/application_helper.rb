adbfc41ed6c38ee21aa18f12561854134fb3e75f
Ken Nishimura <ken97531@gmail.com>
2011-03-04 21:40:24 +0900

    set title for each page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
 
+  def title(page_title)
+    content_for(:title) { page_title }
+  end
+
   def link_to_hatena(url)
     base_url = "http://b.hatena.ne.jp/entry/"
     if url =~ /^(http:\/\/)(.+)/
