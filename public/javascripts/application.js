2c0bac9ddc418ee368d1a9bc1254cab592dfc559
Ken Nishimura <ken97531@gmail.com>
2011-03-30 10:15:29 +0900

    add jquery-ui css and images

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,6 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-$(function() {
-    $('#flash_error').dialog({draggable: true, title: "Error", buttons: { "OK": function() { $(this).dialog("close");}}});
-    $('#flash_notice').dialog({draggable: true, title: "Message", buttons: { "OK": function() { $(this).dialog("close");}}});
-});
+// $(function() {
+//     $('#flash_error').dialog({draggable: true, title: "Error", buttons: { "OK": function() { $(this).dialog("close");}}});
+//     $('#flash_notice').dialog({draggable: true, title: "Message", buttons: { "OK": function() { $(this).dialog("close");}}});
+// });
