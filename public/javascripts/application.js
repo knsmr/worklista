66e7bc6b9b9fec0ce8abc99232ec6765ca53aaae
Ken Nishimura <ken97531@gmail.com>
2011-03-31 21:47:26 +0900

    remove unnecessary comments

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,9 +1,5 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-// $(function() {
-//     $('#flash_error').dialog({draggable: true, title: "Error", buttons: { "OK": function() { $(this).dialog("close");}}});
-//     $('#flash_notice').dialog({draggable: true, title: "Message", buttons: { "OK": function() { $(this).dialog("close");}}});
-// });
 $(function() {
     $('#url-form form input[type=text]').focus();
 });
