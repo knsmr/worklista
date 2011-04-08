30f142b05355ef8948663340292235e933a183cc
Ken Nishimura <ken97531@gmail.com>
2011-04-08 21:05:58 +0900

    reject a failed captcha trial and redirect to the signup page again.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,14 +1,12 @@
 class RegistrationsController < Devise::RegistrationsController
-
   def create
     if recaptcha_valid?
       super
     else
       build_resource
       clean_up_passwords(resource)
-      flash[:alert] = "There was an error with the recaptcha code below. Please re-enter the code and click submit."
-      render_with_scope :new
+      flash[:error] = "There was an error with the message you read from the image below. Please re-enter."
+      redirect_to "/signup"
     end
   end
-
 end
