890eeff30d198e567741da3a931218b319c2fb62
Ken Nishimura <ken97531@gmail.com>
2011-04-20 10:53:27 +0900

    highlight items when mouse is hovering

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,8 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
 $(function() {
     $('#url-form form input[type=text]').focus();
+
+    $('.item').hover(
+	function(){$(this).css('background', '#dfffff')},
+	function(){$(this).css('background', '#f3ffff')}
+    );
 });
