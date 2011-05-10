d9331589a7a378a95da0acfb7a9e57bce088e045
Ken Nishimura <ken97531@gmail.com>
2011-05-10 22:28:17 +0900

    top page redesign

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -8,8 +8,7 @@ $(function() {
 	    $(this).css('background', '#f3ffff')
 	});
 
-    $('ul li.button').button().css({'background': '#cbedff', 'font-size': '1.4em'});
-    $('ul li.button').button();
+    $('.button').button();
     $('#menu-navi ul li a').button();
     $('#item-tab ul li a').button();
     $('#item-tab .selected a').css('background', '#f3ffff').css('border-bottom', 'none');
