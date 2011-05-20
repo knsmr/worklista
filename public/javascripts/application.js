01f1b28cb0a96381f741dc669dedd6b76e7027ab
Ken Nishimura <ken97531@gmail.com>
2011-05-20 15:45:23 +0900

    delay the edit dialog pop up

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -82,7 +82,9 @@ $(function() {
     	.live('ajax:complete', function(evn, xhr, status){
 	    $('input[name=commit]').removeAttr('disabled');
 	    $('input[type=text]').removeAttr('disabled').css('color', 'black');
-	    $('.edit-buttons').first().find('.item_edit').click();
+	    setTimeout(function(){
+		$('.edit-buttons').first().find('.item_edit').click();
+	    }, 500);
 	})
 	.live('ajax:error', function(evn, xhr, status, error){    
 	    $("#new_item").hide(500, function(){$(this).remove();});
