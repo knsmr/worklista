37296d0a2aeb8b25ed3777e3f713e8a30bd35511
Ken Nishimura <ken97531@gmail.com>
2011-06-29 21:34:21 +0900

    request modest permission on facebook oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'app_id', 'app_secret'
-  provider :facebook, 'app_id', 'app_secret'
+  provider :facebook, 'app_id', 'app_secret', :scope => ''
 end
