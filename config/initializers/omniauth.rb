e6a67ee9b5e9324ea731274d5b1ab3dace2da069
Ken Nishimura <ken97531@gmail.com>
2011-08-30 20:37:23 +0900

    use https with oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,13 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'app_id', 'app_secret'
   provider :facebook, 'app_id', 'app_secret', :scope => ''
+  if Rails.env.production?
+    provider :facebook, 'app_id', 'app_secret', {:scope => '', :client_options => {:ssl => { :ca_file => 'you_path_to_ca_certificaets'}}}
+  else
+    provider :facebook, 'app_id', 'app_secret', :scope => ''
+  end
+end
+
+if Rails.env.production?
+  OmniAuth.config.full_host = 'https://worklista.com'
 end
